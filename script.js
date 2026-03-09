c74cbf76890b5e9a81e1d57f4586da690ccb6054
Md Mofazzal Hossain <mhmahdi13813@gmail.com>
2026-03-09 12:39:56 +0600

    Tab Toggles And Tab Button Active Color Function

--- script.js
+++ script.js
@@ -25,3 +25,35 @@ function calculateCount() {
   rejectedCount.innerText = rejectedList.length;
 }
 calculateCount();
+
+
+
+//tab toggle with color function
+function toggleStyle(id) {
+  //remove btn-active
+  allFilterBtn.classList.remove("btn-active");
+  interviewFilterBtn.classList.remove("btn-active");
+  rejectedFilterBtn.classList.remove("btn-active");
+
+  //find the click btn
+  let selected = document.getElementById(id); //click btn
+  currentStatus = id;
+
+  //add btn-active
+  selected.classList.add("btn-active");
+
+  //filter tab while clicking
+
+  if (id == "all-filter-btn") {
+    allCards.classList.remove("hidden");
+    filterSection.classList.add("hidden");
+  } else if (id == "interview-filter-btn") {
+    allCards.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+    renderInterview();
+  } else if (id == "rejected-filter-btn") {
+    allCards.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+    renderRejected();
+  }
+}
